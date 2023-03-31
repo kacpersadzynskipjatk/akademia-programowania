b4a5b3b5eebb6e4e4da2c13484b15187fed903cf
KacperSadzynski <kacper.sadzynski@gmail.com>
2023-03-31 02:01:34 +0200

    Move logic from main file to redditFetcher file

--- Golang/zadania/reddit/fetcher/redditFetcher.go
+++ Golang/zadania/reddit/fetcher/redditFetcher.go
@@ -2,7 +2,12 @@ package fetcher
 
 import (
 	"context"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
 	"io"
+	"net/http"
+	"time"
 )
 
 type Response struct {
@@ -19,4 +24,57 @@ type Response struct {
 type RedditFetcher interface {
 	Fetch(context.Context) (Response, error)
 	Save(io.Writer) error
+}
+
+type Fetcher struct {
+	c      *http.Client
+	host   string
+	output Response
+}
+
+func NewFetcher(host string, t time.Duration) *Fetcher {
+	return &Fetcher{
+		host: host,
+		c: &http.Client{
+			Timeout: t,
+			Transport: &http.Transport{
+				TLSNextProto: map[string]func(authority string, c *tls.Conn) http.RoundTripper{},
+			},
+		},
+	}
+}
+
+func (e *Fetcher) Fetch(ctx context.Context) (Response, error) {
+	ctx = context.WithValue(ctx, "requestID", time.Now().Unix())
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, e.host, http.NoBody)
+	if err != nil {
+		return Response{}, fmt.Errorf("cannot create request: %w", err)
+	}
+	req.Header.Set("User-Agent", "Custom Agent")
+	resp, err := e.c.Do(req)
+	if err != nil {
+		return Response{}, fmt.Errorf("cannot get data: %w", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return Response{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+	var data Response
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return Response{}, fmt.Errorf("cannot unmarshal data: %w", err)
+	}
+	e.output = data
+	return data, nil
+}
+func (e *Fetcher) Save(w io.Writer) error {
+	//CREATE A TXT FILE
+	for _, child := range e.output.Data.Children {
+		d := fmt.Sprintf("%s\n%s\n\n", child.Data.Title, child.Data.URL)
+		_, err := w.Write([]byte(d))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- Golang/zadania/reddit/main.go
+++ Golang/zadania/reddit/main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"context"
-	"crypto/tls"
-	"encoding/json"
-	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"os"
 	"reddit/fetcher"
 	"sync"
@@ -31,7 +27,7 @@ var wg sync.WaitGroup
 
 func run(f fetcher.RedditFetcher, w io.Writer, hostUrl, fileName string) {
 	defer wg.Done()
-	f = NewFetcher(hostUrl, time.Second*3)
+	f = fetcher.NewFetcher(hostUrl, time.Second*3)
 	_, err := f.Fetch(context.Background())
 	if err != nil {
 		log.Fatal(err)
@@ -46,57 +42,4 @@ func run(f fetcher.RedditFetcher, w io.Writer, hostUrl, fileName string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-type Fetcher struct {
-	c      *http.Client
-	host   string
-	output fetcher.Response
-}
-
-func NewFetcher(host string, t time.Duration) *Fetcher {
-	return &Fetcher{
-		host: host,
-		c: &http.Client{
-			Timeout: t,
-			Transport: &http.Transport{
-				TLSNextProto: map[string]func(authority string, c *tls.Conn) http.RoundTripper{},
-			},
-		},
-	}
-}
-
-func (e *Fetcher) Fetch(ctx context.Context) (fetcher.Response, error) {
-	ctx = context.WithValue(ctx, "requestID", time.Now().Unix())
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, e.host, http.NoBody)
-	if err != nil {
-		return fetcher.Response{}, fmt.Errorf("cannot create request: %w", err)
-	}
-	req.Header.Set("User-Agent", "Custom Agent")
-	resp, err := e.c.Do(req)
-	if err != nil {
-		return fetcher.Response{}, fmt.Errorf("cannot get data: %w", err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return fetcher.Response{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-	}
-	var data fetcher.Response
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	if err != nil {
-		return fetcher.Response{}, fmt.Errorf("cannot unmarshal data: %w", err)
-	}
-	e.output = data
-	return data, nil
-}
-func (e *Fetcher) Save(w io.Writer) error {
-	//CREATE A TXT FILE
-	for _, child := range e.output.Data.Children {
-		d := fmt.Sprintf("%s\n%s\n\n", child.Data.Title, child.Data.URL)
-		_, err := w.Write([]byte(d))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
